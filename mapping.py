39c39e29ebd352824e066559e9b003167c6ef4c5
orgacac <orgacac@gmail.com>
2021-01-04 22:02:36 +0530

    new develop push

--- mapping.py
+++ mapping.py
@@ -1,24 +1,22 @@
 
-voice_ouput = {
-            "Hi ! I'm Janice, calling on behalf of an independent financial advisory firm regarding Careshield life. Currently we are spreading awareness regarding government disability income scheme. Are you aware of it?": "./janice_voice/5.wav",
-            "Careshield life is a government initiative that provides long-term care insurance scheme giving basic financial support should Singaporeans become severely disabled and medical care for a prolonged duration We are doing a free upgrade service to enhanced the payout. Would you be interested to find out more?": "./janice_voice/27.wav",
-            "This is a long term care insurance scheme. That offers payout upon severe disability": "./janice_voice/11.wav",
-            "This fully payable by Medisave if your'e between the age(30-67)": "./janice_voice/22.wav",
-            "Thats great, we will have our professional agent to follow up with you on this.Thank you": "./janice_voice/7.wav",
-            "would you like to arrange another call with [Chinese] speaking agent?": "./janice_voice/0.wav",
-            "will it be alright to call you back?": "./janice_voice/13.wav",
-            "Alright,thank you for picking up the call and sorry to disturb goodbye": "./janice_voice/10.wav",
-            "NOted.Will send an e-brochure instead.thank you.": "./janice_voice/17.wav",
-            "Thank you for your time. Goodbye.": "./janice_voice/21.wav",
-            "Thanks for picking up the call,our professionals will call you after working hours.": "./janice_voice/18.wav",
-            "Hello, I'm Janice calling on behalf of Aviva regarding Careshield life. Are you aware of it?": "./janice_voice/20.wav",
-            "thats great! will you be interested in our free upgrade as well? Don't worry, you will not need to pay for anything": "./janice_voice/32.wav",
-            "oh! why not ?": "./janice_voice/30.wav",
-            "Careshield life is a disability income scheme launched by the government. As long as you are unable to move around the house, the government will be giving you money every month.Are you interested?": "./janice_voice/31.wav",
-            "sounds good. Thank you for your time. Goodbye": "./janice_voice/29.wav",
-            "Sorry, I didn't get what you said. Please rephrase what you said.": "./janice_voice/28.wav",
-            "Thank you for expressing your interest, we will get our human representative to contact you shortly.": "./janice_voice/26.wav",
-            "Hi ! I'm Janice, calling on behalf of an independent financial advisory firm regarding Careshield life.Currently we are spreading awareness regarding government disability income scheme. Are you aware of it?": "./janice_voice/25.wav"
+voice_output = {
+            "greet_info": "Hi ! I'm Janice, calling on behalf of an independent financial advisory firm regarding Careshield life.Currently we are spreading awareness regarding government disability income scheme. Are you aware of it?",
+            "info": "Careshield life is a government initiative that provides long-term care insurance scheme giving basic financial support should Singaporeans become severely disabled and medical care for a prolonged duration We are now doing a free upgrade service to enhanced the payout. Would you be interested to find out more?",
+            "offers_info": "It is a national long term care insurance scheme.offers payout upon severe disability",
+            "payable_info": "fully payable by Medisave(30-67) will it be alright if our prfessionals contact you again to tell you more?",
+            "language_check": "would you like to arrange another call with [mandarin] speaking professional?",
+            "call_again_check": "will it be alright if i call you back some time later this week?",
+            "sad_bye": "Alright,thanks for picking up the call and sorry to disturb",
+            "ebrochure_check": "Noted.Will send an e-brochure instead.thank you.",
+            "goodbye": "Thank you for your time. Goodbye.",
+            "happy_bye": "Thanks for picking up the call,our professionals will call you after working hours.",
+            "greet": "Hello, I'm Janice calling on behalf of Aviva regarding your Careshield life. Are you aware of it?",
+            "upgrade_check": "thats great! will you be interested in our free upgrade as well? Don't worry, you will not need to pay for anything",
+            "reason_check": "oh! why not ?",
+            "more_info": "Careshield life is a disability income scheme launched by the government. As long as you are unable to move around the house, the government will be giving you money every month.Are you interested?",
+            "sounds good. Thank you for your time. Goodbye": "./audio_clips/29.wav",
+            "out_of_scope": "Sorry, I didn't get what you said. Please rephrase what you said.",
+            "contact_representative": "Thank you for expressing your interest, we will get our human representative to contact you shortly.",     
         }
 
 
--- socketio_connector.py
+++ socketio_connector.py
@@ -23,7 +23,7 @@ import time
 import numpy as np
 from collections import OrderedDict
 import urllib
-from mapping import voice_ouput
+from mapping import voice_output
 import librosa
 
 #from TTS.models.tacotron import Tacotron
@@ -67,11 +67,11 @@ class SocketIOOutput(OutputChannel):
     #    from gtts import gTTS
      #   myobj = gTTS(text=text, lang='en', slow=False)
       #  myobj.save(OUT_FILE)
-
-    def search_in_dict(self, text, OUT_FILE):
-        for key, value in voice_ouput.items():
-            if text == key:
-                copyfile(value, OUT_FILE)
+    def getText(self,utterance): 
+        for key,value in voice_output.items():
+            if utterance == key:
+                return value 
+            #copyfile('./audio_clips/'+ text + '.wav', OUT_FILE)
 
     async def _send_audio_message(self, socket_id, response,  **kwargs: Any):
         # type: (Text, Any) -> None
@@ -80,8 +80,9 @@ class SocketIOOutput(OutputChannel):
         OUT_FILE = str(ts)+'.wav'
         link = "http://localhost:8888/"+OUT_FILE
         #self.tts(response['text'], OUT_FILE)
-        self.search_in_dict(response['text'], OUT_FILE)
-        await self.sio.emit(self.bot_message_evt, {'text':response['text'], "link":link}, room=socket_id)
+        copyfile('./audio_clips/'+ response['text'] + '.wav', OUT_FILE)
+        responseText = self.getText(response['text'])
+        await self.sio.emit(self.bot_message_evt, {'text':responseText, "link":link}, room=socket_id)
 
 
     async def send_text_message(self, recipient_id: Text, message: Text, **kwargs: Any) -> None:

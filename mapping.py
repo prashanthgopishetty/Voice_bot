ef947c895a859b368f8ae53963251e348b2f8c32
orgacac <orgacac@gmail.com>
2021-01-04 19:33:21 +0530

    initial develop commit

--- mapping.py
+++ mapping.py
@@ -1,24 +1,22 @@
 
-voice_ouput = {
-            "Hi ! I'm Janice, calling on behalf of an independent financial advisory firm regarding Careshield life. Currently we are spreading awareness regarding government disability income scheme. Are you aware of it?": "./janice_voice/5.wav",
-            "Careshield life is a government initiative that provides long-term care insurance scheme giving basic financial support should Singaporeans become severely disabled and medical care for a prolonged duration We are doing a free upgrade service to enhanced the payout. Would you be interested to find out more?": "./janice_voice/27.wav",
-            "This is a long term care insurance scheme. That offers payout upon severe disability": "./janice_voice/11.wav",
-            "This fully payable by Medisave if your'e between the age(30-67)": "./janice_voice/22.wav",
-            "Thats great, we will have our professional agent to follow up with you on this.Thank you": "./janice_voice/7.wav",
-            "would you like to arrange another call with [Chinese] speaking agent?": "./janice_voice/0.wav",
-            "will it be alright to call you back?": "./janice_voice/13.wav",
-            "Alright,thank you for picking up the call and sorry to disturb goodbye": "./janice_voice/10.wav",
-            "NOted.Will send an e-brochure instead.thank you.": "./janice_voice/17.wav",
-            "Thank you for your time. Goodbye.": "./janice_voice/21.wav",
-            "Thanks for picking up the call,our professionals will call you after working hours.": "./janice_voice/18.wav",
-            "Hello, I'm Janice calling on behalf of Aviva regarding Careshield life. Are you aware of it?": "./janice_voice/20.wav",
-            "thats great! will you be interested in our free upgrade as well? Don't worry, you will not need to pay for anything": "./janice_voice/32.wav",
-            "oh! why not ?": "./janice_voice/30.wav",
-            "Careshield life is a disability income scheme launched by the government. As long as you are unable to move around the house, the government will be giving you money every month.Are you interested?": "./janice_voice/31.wav",
-            "sounds good. Thank you for your time. Goodbye": "./janice_voice/29.wav",
-            "Sorry, I didn't get what you said. Please rephrase what you said.": "./janice_voice/28.wav",
-            "Thank you for expressing your interest, we will get our human representative to contact you shortly.": "./janice_voice/26.wav",
-            "Hi ! I'm Janice, calling on behalf of an independent financial advisory firm regarding Careshield life.Currently we are spreading awareness regarding government disability income scheme. Are you aware of it?": "./janice_voice/25.wav"
+voice_output = {
+            "greet_info": "./audio_clips/greet_info.wav",
+            "info": "./audio_clips/info.wav",
+            "offers_info": "./audio_clips/offers_info.wav",
+            "payable_info": "./audio_clips/payable_info.wav",
+            "language_check": "./audio_clips/language_check.wav",
+            "call_again_check": "./audio_clips/call_again_check.wav",
+            "sad_bye": "./audio_clips/sad_bye.wav",
+            "ebrochure_check": "./audio_clips/ebrochure_check.wav",
+            "goodbye": "./audio_clips/goodbye.wav",
+            "happy_bye": "./audio_clips/happy_bye.wav",
+            "greet": "./audio_clips/greet.wav",
+            "upgrade_check": "./audio_clips/upgrade_check.wav",
+            "reason_check": "./audio_clips/reason_check.wav",
+            "more_info": "./audio_clips/more_info.wav",
+            "sounds good. Thank you for your time. Goodbye": "./audio_clips/29.wav",
+            "out_of_scope": "./audio_clips/out_of_scope.wav",
+            "contact_representative": "./audio_clips/contact_reprentative.wav",     
         }
 
 

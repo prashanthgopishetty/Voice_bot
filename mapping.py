43461cd906b24d51c7fba04d0460c55134419be1
orgacac <orgacac@gmail.com>
2021-02-18 09:50:20 +0530

    new commit

--- mapping.py
+++ mapping.py
@@ -1,22 +1,10 @@
 
 voice_output = {
-            "greet_info": "./audio_clips/greet_info.wav",
-            "info": "./audio_clips/info.wav",
-            "offers_info": "./audio_clips/offers_info.wav",
-            "payable_info": "./audio_clips/payable_info.wav",
-            "language_check": "./audio_clips/language_check.wav",
-            "call_again_check": "./audio_clips/call_again_check.wav",
-            "sad_bye": "./audio_clips/sad_bye.wav",
-            "ebrochure_check": "./audio_clips/ebrochure_check.wav",
-            "goodbye": "./audio_clips/goodbye.wav",
-            "happy_bye": "./audio_clips/happy_bye.wav",
-            "greet": "./audio_clips/greet.wav",
-            "upgrade_check": "./audio_clips/upgrade_check.wav",
-            "reason_check": "./audio_clips/reason_check.wav",
-            "more_info": "./audio_clips/more_info.wav",
-            "sounds good. Thank you for your time. Goodbye": "./audio_clips/29.wav",
-            "out_of_scope": "./audio_clips/out_of_scope.wav",
-            "contact_representative": "./audio_clips/contact_reprentative.wav",     
+            "greet_info": "Hi, This is Janice, calling from propnex a property agency. Is this a good time to speak now?",
+            "purpose": "We are reaching out to home owners that are interested to find out more on their current market potential of their properties. In this covid period, many owners out there are selling their properties at a over valued price. We would like to offer you a free valuation report to let you understand your current property value. Would you be keen?",
+            "details": "Yes, so I would like to find out first are you currently staying in a private or hdb flat right now?",
+            "contact_agent": "Great! That will do. Our professional agent will contact you regarding the report. Thank you so much for your time.",
+            "thanks_bye": "Yups. We will be in touch with you shortly. Thank you so much for your time. Goodbye.",
+            "bye": "ok thank you. Goodbye.",
+            "goodbye": "okay thank you so much for your time. Goodbye.",
         }
-
-
